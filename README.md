ea28d7812f44c767840139f196e8f5369f75ffee
Ali <gusarual@student.gu.se>
2020-02-23 17:45:21 +0100

    Assignement

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # LT2212 V20 Assignment 1
 
-Put any documentation here including any answers to the questions in the 
-assignment on Canvas.
+
+Part1: 
+
+I outputs and save all the files' contents in a one string corpus, and in a dictionary in the same loop. The dictionary contains all the files as keys, with the file content's as the values. The count all the occurrences of the words in the corpus to determine which words to include in the dataframe, if their count is more than n times. Then I iterate through the dict one by own using the index position of the files in data frame: [position,"classes"]+"/"+B.loc[position,"files"]], and count the occurrence of each word in the given file.  The data is lowered, and I have not removed punctuation and numbers, so non-alphabetic characters will appear in the data frame. 
+
+
+Part2 : 
+
+step1:I make a copy of the data frame, excluding the column of the file names. Step:2 I group all the the rows into two columns with sum total of each word in each class. Step:3 I add a new row of the total count of the word in both classes in order to sort them. Step 4: I sort the dataframe based on the total sum. Step5: A copy of the sorted data-frame with column limit of the most m occurred words. 
+
+
+Part3: 
+
+Step1: a copy of the data frame without the files and the classes. Step2: the length of dataframe- or the length of the columns which is equivalent to the the total number of all files. Step3: I get the idf. Step4: I get the tf-idf, the counting times idf. Step:5 a copy of the two columns, classes and files. Step6: I join the the classes and files to the new tf-idf to return the final and full data frame. 
+
+
+Part4: 
+The frequency of the top used words has changed dramatically, with more words that has a remarkable difference in representation from one class to the other. I assume this change is due to the relative ratio that that tf-idf points, represented by narrower numbers or narrower classification of 0 or above 0. 
+
+
+Part Bosun- classiy: 
+
+I used logistic regression to measure the accuracy. Step1: Choosing the feature. Step2: label. The accuracy of part1 to tf-if varied in the beginning. One time it was better for part 1 and other time was better accuracy for tf-idf, I kept changing the train and test size of the data, and also the random state, once zero and once 1. I could not figure out why it differ here. One strange thing I noticed that when I expanded the train size the accuracy went lower, which I assumed the case should be otherwise, meaning that If the training data is larger it should produce more accuracy on the test data. But perhaps I am doing something wrong. I also did not shuffle the the data. The accuracy results were between 0.95-0.96
--- a1.py
+++ a1.py
@@ -3,27 +3,75 @@ import sys
 import pandas as pd
 import numpy as np
 import numpy.random as npr
-# ADD ANY OTHER IMPORTS YOU LIKE
-
-# DO NOT CHANGE THE SIGNATURES OF ANY DEFINED FUNCTIONS.
-# YOU CAN ADD "HELPER" FUNCTIONS IF YOU LIKE.
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
 
 def part1_load(folder1, folder2, n=1):
-    # CHANGE WHATEVER YOU WANT *INSIDE* THIS FUNCTION.
-    return pd.DataFrame(npr.randn(2,2)) # DUMMY RETURN
+    allfiles = glob("{}/*.txt".format(folder1)) + glob("{}/*.txt".format(folder2))
+    
+    #spliting the file names for the first two columns
+    classes_and_files=[i.split("/") for i in allfiles]
+    B=pd.DataFrame({"files":[i[1]for i in classes_and_files],"classes":[i[0]for i in classes_and_files]})
+    
+    corpus = ""
+    data_in_files={}
+    for filename in allfiles:
+        data_in_files[filename]=""
+        with open(filename, "r") as thefile:
+            for line in thefile:
+                corpus += "\n" + line
+                data_in_files[filename]+=line.lower()
 
-def part2_vis(df):
+    p= corpus.lower().split()
+    #counting each word in the corpus using numpy, ouputs an array of tuples (word,count)
+    unique, counts= np.unique(p, return_counts=True)
+    counted_words=np.asarray((unique, counts)).T
+    
+    indexes= range(0,len(B["classes"]))
+    for i in counted_words:
+        if int(i[1])>n:     #if the word is mentioned more than n times in the etnire corpus
+            counts=[]
+            for position in indexes:      # count the occurance of a word in each file
+                c=data_in_files[B.loc[position,"classes"]+"/"+B.loc[position,"files"]].lower().split()
+                counts.append(c.count(i[0]))
+            B[i[0]]=[int(i) for i in counts]
+
+    return B # DUMMY RETURN
+df=loader_data("crude","grain",1000)
+def part2_vis(df,m):
     # DO NOT CHANGE
     assert isinstance(df, pd.DataFrame)
-
-    # CHANGE WHAT YOU WANT HERE
-    return df.plot(kind="bar")
+    # DO NOT CHANGE
+    assert isinstance(df, pd.DataFrame)
+    # a copy of the dataframe using iloc, excluding the column of the filename
+    R=df.iloc[:,1:]
+    R= R.groupby(["classes"]).sum()
+    i= R.append(R.agg(["sum"]))
+    h=i.T.sort_values("sum",ascending=False).T
+    t=h.iloc[: , 0:m]             #sorted dataframe exluding the total raw and including top m
+    
+    return t.T.plot(kind="bar")
 
 def part3_tfidf(df):
     # DO NOT CHANGE
     assert isinstance(df, pd.DataFrame)
-
-    # CHANGE WHAT YOU WANT HERE
-    return df #DUMMY RETURN
-
-# ADD WHATEVER YOU NEED HERE, INCLUDING BONUS CODE.
+    t=df.iloc[: ,2:]    # a copy without calsses and files
+    number_of_allfiles=len(df)
+    v= np.log(number_of_allfiles/t[t>0].count())
+    b= t * np.array(v)   # tf times idf
+    f=df.iloc[:,:2]      #files and classes
+    c=pd.concat([f,b],axis=1)  #returning classes and files to the new tf-idf dataframe
+    
+    return c #DUMMY RETURN
+def classifier_data(T):
+    
+    X=T.iloc[:,2:]
+    y=T["classes"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y,train_size=0.75,test_size=0.25,random_state=0)
+    logreg = LogisticRegression()
+    logreg.fit(X_train, y_train)
+    y_pred_class= logreg.predict(X_test)
+    v=metrics.accuracy_score(y_test, y_pred_class)
+    return v
